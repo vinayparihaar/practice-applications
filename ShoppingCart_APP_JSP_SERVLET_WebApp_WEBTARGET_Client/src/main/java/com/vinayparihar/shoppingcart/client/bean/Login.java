6cf4788d62a7cbe51279bff8968a8da5e82c7288
vinayparihaar <vinaythakuur@gmail.com>
2020-05-31 15:33:14 -0400

    ShoppingCart APP JSP SERVLET WebApp WEBTARGET Client Version=01

---
+++ ShoppingCart_APP_JSP_SERVLET_WebApp_WEBTARGET_Client/src/main/java/com/vinayparihar/shoppingcart/client/bean/Login.java
@@ -0,0 +1,59 @@
+package com.vinayparihar.shoppingcart.client.bean;
+
+/**
+ * @author VinayParihar
+ * @date Apr 29, 2020 @time 2:27:13 PM 
+ */
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "email", "password" })
+public class Login {
+
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("password")
+	private String password;
+
+	/**
+	 * No args constructor for use in serialization
+	 *
+	 */
+	public Login() {
+	}
+
+	/**
+	 *
+	 * @param password
+	 * @param email
+	 */
+	public Login(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
+	@JsonProperty("email")
+	public String getEmail() {
+		return email;
+	}
+
+	@JsonProperty("email")
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@JsonProperty("password")
+	public String getPassword() {
+		return password;
+	}
+
+	@JsonProperty("password")
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
